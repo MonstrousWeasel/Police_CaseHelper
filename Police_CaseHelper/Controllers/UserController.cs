4ee22d10ce069f07d0cc85a0d5cbed9e836f6d6d
Jessica Anderson <Jessica.Anderson@outlook.co.nz>
2023-10-14 21:54:24 +1300

    Can now edit a user and their roles (all in one screen)

--- Police_CaseHelper/Controllers/UserController.cs
+++ Police_CaseHelper/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Police_CaseHelper.Areas.Identity.Data;
 using Police_CaseHelper.Core;
 using Police_CaseHelper.Core.Repositories;
+using Police_CaseHelper.Core.ViewModels;
 
 namespace Police_CaseHelper.Controllers
 {
@@ -23,6 +25,86 @@ namespace Police_CaseHelper.Controllers
         {
             var users = _unitOfWork.User.GetUsers();
             return View(users);
+        }
+
+        [Authorize(Policy = Constants.Policies.RequireAdmin)]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = _unitOfWork.User.GetUser(id);
+            var roles = _unitOfWork.Role.GetRoles();
+
+            var userRoles = await _signInManager.UserManager.GetRolesAsync(user);
+
+            var roleItems = roles.Select(role =>
+                new SelectListItem(
+                    role.Name,
+                    role.Id,
+                    userRoles.Any(ur => ur.Contains(role.Name)))).ToList();
+
+            var vm = new EditUserViewModel
+            {
+                User = user,
+                Roles = roleItems
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostAsync(EditUserViewModel data)
+        {
+            var user = _unitOfWork.User.GetUser(data.User.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRolesInDb = await _signInManager.UserManager.GetRolesAsync(user);
+
+            //Loop through the roles in ViewModel
+            //Check if the Role is Assigned In DB
+            //If Assigned -> Do Nothing
+            //If Not Assigned -> Add Role
+
+            var rolesToAdd = new List<string>();
+            var rolesToDelete = new List<string>();
+
+            foreach (var role in data.Roles)
+            {
+                var assignedInDb = userRolesInDb.FirstOrDefault(ur => ur == role.Text);
+                if (role.Selected)
+                {
+                    if (assignedInDb == null)
+                    {
+                        rolesToAdd.Add(role.Text);
+                    }
+                }
+                else
+                {
+                    if (assignedInDb != null)
+                    {
+                        rolesToDelete.Add(role.Text);
+                    }
+                }
+            }
+
+            if (rolesToAdd.Any())
+            {
+                await _signInManager.UserManager.AddToRolesAsync(user, rolesToAdd);
+            }
+
+            if (rolesToDelete.Any())
+            {
+                await _signInManager.UserManager.RemoveFromRolesAsync(user, rolesToDelete);
+            }
+
+            user.FirstName = data.User.FirstName;
+            user.Surname = data.User.Surname;
+            user.Email = data.User.Email;
+
+            _unitOfWork.User.UpdateUser(user);
+
+            return RedirectToAction("Edit", new { id = user.Id });
         }
     }
 }
---
+++ Police_CaseHelper/Core/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Police_CaseHelper.Areas.Identity.Data;
+
+namespace Police_CaseHelper.Core.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public ApplicationUser  User { get; set; }
+        public IList<SelectListItem> Roles { get; set; }
+    }
+}
