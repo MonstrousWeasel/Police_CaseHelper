3456a420a6312133ab822aeda6d4b21293bb36ed
Jessica Anderson <Jessica.Anderson@outlook.co.nz>
2023-10-15 17:14:16 +1300

    Fixed up cases, so it only has the index and details

--- Police_CaseHelper/Controllers/CasesController.cs
+++ Police_CaseHelper/Controllers/CasesController.cs
@@ -55,58 +55,6 @@ namespace Police_CaseHelper.Controllers
         } 
         #endregion
 
-
-        // GET: Cases/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Cases == null)
-            {
-                return NotFound();
-            }
-
-            var cases = await _context.Cases.FindAsync(id);
-            if (cases == null)
-            {
-                return NotFound();
-            }
-            return View(cases);
-        }
-
-        // POST: Cases/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CaseID,VictimName,OffenderName,Charges,OffenderPlea,OffenderInCustody,OffenderReleaseDate,CourtDates,CourtLocation,Details,Bail,BailConditions,BailBreached,RequiredToBeArrested,WantedToArrest,OfficerInChargeName,OfficerInChargeEmail,OfficerInChargePhone,VictimUserName")] Cases cases)
-        {
-            if (id != cases.CaseID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cases);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CasesExists(cases.CaseID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cases);
-        }
-
         #region CasesExists
         private bool CasesExists(int id)
         {
